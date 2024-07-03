65991f3097ceb01fb2a62800b6690f949a1b7a1c
jeffbarsoum <143226227+jeffbarsoum@users.noreply.github.com>
2024-07-02 21:25:08 -0400

    Code written (needs to be tested)

--- linked_lists.rb
+++ linked_lists.rb
@@ -1,51 +1,68 @@
 class LinkedList
-  def initialize
+  # setters and getters for first and last node in the list
+  attr_accessor :head, :tail
 
+  def initialize value
+    prepend(value)
   end
 
   # adds a new node containing value to the end of the list
   def append(value)
-
+    new_node = self.tail = Node.new(value)
+    self.tail.next_node = new_node
+    self.tail = new_node
   end
 
   # adds a new node containing value to the start of the list
   def prepend(value)
-
+    self.head = Node.new(value, self.head)
   end
 
   # returns the total number of nodes in the list
   def size
-
-  end
-
-  # returns the first node in the list
-  def head
-
-  end
-
-  # returns the last node in the list
-  def tail
-
+    cnt = self.head ? 1: 0
+    next_pointer = self.head
+    while next_pointer do
+      if next_pointer.next_node
+        next_pointer = next_pointer.next_node
+        cnt += 1
+      end
+    end
+    cnt
   end
 
   # returns the node at the given index
   def at(index)
-
+    cnt = self.head ? 1 : 0
+    next_pointer = self.head
+    return self.head if index == 0
+    while cnt < index do
+      cnt += 1
+      next_pointer = next_pointer.next_node
+    end
   end
 
   # removes the last element from the list
   def pop
-
+    result = at(size).value
+    at(size - 1).next_node = nil
+    result
   end
 
   # returns true if the passed in value is in the list and otherwise returns false.
   def contains?(value)
-
+    next_pointer = self.head
+    until is_contains do
+      return true if next_pointer.value == value
+      next_pointer = next_pointer.next_node
+    end
   end
 
   # returns the index of the node containing value, or nil if not found.
   def find(value)
-
+    self.size.times do |i|
+      return at(i) if at(i).value == value
+    end
   end
 
   # represent your LinkedList objects as strings,
@@ -53,7 +70,10 @@ class LinkedList
   #
   # The format should be: ( value ) -> ( value ) -> ( value ) -> nil
   def to_s
-
+    str = ""
+    self.size.times do |i|
+      str += "( #{at(i).value} ) -> "
+    end
   end
 
   ####################
@@ -62,26 +82,23 @@ class LinkedList
 
   # inserts a new node with the provided value at the given index.
   def insert_at(value, index)
-
+    at(index - 1).next_node = Node.new(value, at(index))
   end
 
   # removes the node at the given index
   def remove_at(index)
-
+    at(index - 1).next_node = at(index + 1)
   end
 
 end
 
 class Node
-  attr_accessor :head, :tail, :value
+  # setters and getters for "value" and "next_node"
+  attr_accessor :value, :next_node
 
-  def initialize
-    @head = nil
-    @tail = nil
-    @value = nil
+  def initialize val, nxt_nd = nil
+    self.value = val
+    self.next_node = nxt_nd
   end
 
-  def next_node
-
-  end
 end
